fb538cc69d43a400e833f9711e9270c3817bd592
Ilidan-Sugars <iljasaharov@ya.ru>
2025-09-16 17:11:41 +0700

    index +scss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,29 @@
 @extends('layouts.app')
 
-@section('title', 'Home')
+@section('title', 'Главная')
 
 @section('content')
-
-    <ul>
-        @foreach ($videos as $video)
-            <li>{{ $video->video_name }}</li>
-        @endforeach
+<div class="home-container">
+    <ul class="video-list">
+        @forelse ($videos as $video)
+            <li class="video-item">
+                <div class="video-thumb">
+                    @if($video->video_thumbnail)
+                        <img src="{{ $video->video_thumbnail }}" alt="{{ $video->video_name }} thumbnail">
+                    @else
+                        <div class="no-thumb"> </div>
+                    @endif
+                </div>
+                <div class="video-info">
+                    <span class="video-title">{{ $video->video_name }}</span>
+                    @if($video->video_description)
+                        <p class="video-desc">{{ $video->video_description }}</p>
+                    @endif
+                </div>
+            </li>
+        @empty
+            <li class="no-videos">Видео не найдены.</li>
+        @endforelse
     </ul>
-
-
+</div>
 @endsection
